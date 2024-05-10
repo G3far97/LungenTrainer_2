b15318ce9c267cb56e73424e9feff1949924a608
Ahmed Gaafar <47535864+G3far97@users.noreply.github.com>
2024-05-10 04:12:39 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.static('./Lungentrainer_2'));
 
 // If you have an index.html file, you can set up a route like this:
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + './Lungentrainer_2/PackagingLog.html');
+  res.sendFile(__dirname + '/Lungentrainer_2/PackagingLog.html');
 });
 
 // Start the server
