e49fe6f02b9db1eb7ec60a957f752fbc761b177e
Ahmed Gaafar <47535864+G3far97@users.noreply.github.com>
2024-05-10 04:08:47 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000; // Render assigns a port via process.env.
 // Serve your MATLAB web app's static files
 app.use(express.static('./Lungentrainer_2'));
 
+// If you have an index.html file, you can set up a route like this:
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + './Lungentrainer_2/PackagingLog.html');
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
